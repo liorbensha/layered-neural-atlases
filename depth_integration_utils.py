a346d011729d2c46f11dd0c206126144783f0289
Yakir Oz <yakir.oz@weizmann.ac.il>
2022-08-14 14:27:41 +0300

    Before making augmented experiment

--- depth_integration_utils.py
+++ depth_integration_utils.py
@@ -7,80 +7,26 @@ def get_tuples_undistorted(number_of_frames, video_frames, depth_frames,
                            intrisics):
     # video_frames shape: (resy, resx, 3, num_frames), mask_frames shape: (resy, resx, num_frames)
     jif_all = []
+    undistorted_jif_all = []
     depth_all = []
     for f in range(number_of_frames):
         mask = (video_frames[:, :, :, f] > -1).any(dim=2)
-        current_intrisics = intrisics[f]
+        fx, fy, cx, cy = intrisics[f]
         relis, reljs = torch.where(mask > 0.5)
         depth = depth_frames[f][relis, reljs]
         #TODO try positional encoding?
         # now apply intrisic to new data strutcure
+        relis_undistorted = (fx * relis - cx) * depth # TODO(Yakir): make sure that cx and cy are not swapped
+        reljs_undistorted = (fy * reljs - cy) * depth
         depth_all.append(depth)
         jif_all.append(torch.stack((reljs, relis, f * torch.ones_like(reljs))))
-    return torch.cat(jif_all, dim=1), torch.cat(depth_all)
-
-
-def principal_point(intrinsics):
-    """
-    Args:
-        intrinsics: (fx, fy, cx, cy)
-        shape: (H, W)
-    """
-    return intrinsics[:, 2:]
-
-
-def focal_length(intrinsics):
-    return intrinsics[:, :2]
-
-
-def pixels_to_rays(pixels, intrinsics):
-    """Convert pixels to rays in camera space using intrinsics.
-
-    Args:
-        pixels (2, B)
-        intrinsics (4, B): (fx, fy, cx, cy)
-
-    Returns:
-        rays: (3, B), where z component is -1, i.e., rays[:, -1] = -1
-
-    """
-    # Assume principal point is ((W-1)/2, (H-1)/2).
-    _, N = pixels.shape
-    cs = principal_point(
-        intrinsics)  # hopefully will be able to have it for each point
-    # Convert to [-(W-1)/2, (W-1)/2] x [-(H-1)/2, (H-1)/2)] and bottom left is (0, 0)
-    uvs = pixels - cs.view(-1, 2, 1, 1)
-    uvs[:, 1] = -uvs[:, 1]  # flip v
-
-    # compute rays (u/fx, v/fy, -1)
-    fxys = focal_length(intrinsics).view(-1, 2, 1, 1)
-    rays = torch.cat((uvs / fxys, -torch.ones(
-        (B, 1, H, W), dtype=uvs.dtype, device=_device)),
-                     dim=1)
-    return rays
-
-
-def pixels_to_points(intrinsics, depths, pixels):
-    """Convert pixels to 3D points in camera space. (Camera facing -z direction)
-
-    Args:
-        intrinsics:
-        depths (B, 1)
-        pixels (B, 2)
-
-    Returns:
-        points (B, 3)
-
-    """
-    rays = pixels_to_rays(pixels, intrinsics)
-    points = rays * depths
-    return points
+        undistorted_jif_all.append(torch.stack((reljs_undistorted, relis_undistorted, f * torch.ones_like(reljs))))
+    return torch.cat(jif_all, dim=1), torch.cat(depth_all), torch.cat(undistorted_jif_all)
 
 
 class CoordinatesWithDepth():
     def __init__(self, video_frames, depth_frames, intrisic_parameters):
         """
-        expecteing `intrisic_parameters` of shape (B, 4) (with repetitions of course)
         """
 
         # Save input
@@ -88,19 +34,21 @@ class CoordinatesWithDepth():
         self.depth_frames = depth_frames
 
         # Compute the needed data
-        self.jif_all, self.depth_at_jif = get_tuples(video_frames.shape[3],
+        self.jif_all, self.depth_at_jif, self.undistorted_jif_all = get_tuples_undistorted(video_frames.shape[3],
                                                      video_frames,
-                                                     depth_frames)
+                                                     depth_frames, intrisic_parameters)
         # jif_all.shape = (3, num_points)
-        # depth_at_jif.shape = (num_points) - figure out how to apply K^-1 efficently to not get memory out
+        # depth_at_jif.shape = (num_points)
+        # self.undistorted_dx, self.undistorted_dy = self.get_undistorted_gradient(
+        #     intrisic_parameters)
 
-        self.undistorted_jif_all, self.undistorted_depth_at_jif = self.undistort_jif_and_depth(
-            intrisic_parameters)
-        self.undistorted_dx, self.undistorted_dy = self.get_undistorted_gradient(
-            intrisic_parameters)
+    # def get_undistorted_gradient(self, intrisic_parameters):
+    #     video_frames_dx = torch.zeros((resy, resx, 3, number_of_frames))
+    #     video_frames_dy = torch.zeros((resy, resx, 3, number_of_frames))
+    #     video_frames = self.video_frames
+    #     # TODO: need undistorted frames here... Or not? ask Ronen and Meirav
+    #     for i in range(number_of_frames):
+    #         video_frames_dy[:-1, :, :,i] = video_frames[1:, :, :, i] - video_frames[:-1, :, :, i]
+    #         video_frames_dx[:, :-1, :,i] = video_frames[:, 1:, :, i] - video_frames[:, :-1, :, i]
 
-    def undistort_jif_and_depth(self, intrisic_parameters):
-        return None, None
-
-    def get_undistorted_gradient(self, intrisic_parameters):
-        return None, None
+    #     video_frames_dx, video_frames_dy
